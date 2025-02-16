3fa0d995e83b38ca145c761922d4acf8e177148c
kirillidk <k.mironov03@gmail.com>
2025-02-16 21:35:44 +0300

    project setup

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <custom_stl_vector.h>
+#include <gtest/gtest.h>
+
+TEST(HelloTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
